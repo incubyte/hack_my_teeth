af4e9246383302a13b6367babf62901a8c71a3d3
Shubham Gangurde <shubhamgangurde16@gmail.com>
2023-11-27 18:12:29 +0530

    fix applied

--- java_server/src/main/java/com/mgmsec/HackMyTeeth/HackMyTeeth/dao/UserRepository.java
+++ java_server/src/main/java/com/mgmsec/HackMyTeeth/HackMyTeeth/dao/UserRepository.java
@@ -1,168 +1,20 @@
 ```java
-package com.mgmsec.HackMyTeeth.HackMyTeeth.dao;
-
-import com.mgmsec.HackMyTeeth.HackMyTeeth.model.User;
-import com.mgmsec.HackMyTeeth.HackMyTeeth.setting.SecurityEnum.PwdStorage;
-import com.mgmsec.HackMyTeeth.HackMyTeeth.setting.SecuritySettings;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-@Repository
-public class UserRepository {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    private EntityManager manager;
-
-    @Autowired
-    SecuritySettings secSetting;
-
-    public List<User> findAll() {
-        try {
-            List<User> result = jdbcTemplate.query("SELECT * from user", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"),rs.getString("salt")));
-
+public List<User> searchDentist(String key) {
+    try {
+        if (!key.equals("")) {
+            String sql = "select * from user where role = 1 AND (firstName LIKE ? OR lastName LIKE ?)";
+            String searchKey = "%" + key + "%";
+            List<User> result = jdbcTemplate.query(sql, (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"), rs.getString("salt")), searchKey, searchKey);
             return result;
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
-        return null;
-    }
-
-    public List<User> listDentist() {
-        try {
-            List<User> result = jdbcTemplate.query("select * from user  where role = 1", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"),rs.getString("salt")));
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public List<User> searchDentist(String key) {
-        try {
-            if (!key.equals("")) {
-                List<User> result = jdbcTemplate.query("select * from user  where role = 1 AND (firstName LIKE ? OR lastName LIKE ?)", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"),rs.getString("salt")), "%" + key + "%", "%" + key + "%");
-                return result;
-            } else {
-                List<User> result = jdbcTemplate.query("select * from user  where role = 1", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"),rs.getString("salt")));
-                return result;
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public String findByUsername(String username, String password) {
-        List<User> result = jdbcTemplate.query("SELECT * FROM user WHERE username=? and password=?", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"),rs.getString("salt")), username, password);
-        String s1 = "0";
-        String s2 = "1";
-        if (result.isEmpty()) {
-            return "invalid";
-        } else if (result.get(0).getRole().equals(s2)) {
-            return "dentist";
-        } else if (result.get(0).getRole().equals(s1)) {
-            return "customer";
-        }
-        return null;
-    }
-
-    public User getDentistById(String id, Boolean sqli) {
-        List<User> result;
-        if (sqli) {
-            int param;
-            try {
-                param = Integer.valueOf(id);
-            } catch (Exception e) {
-                e.printStackTrace();
-                param = 0;
-            }
-            String asql = "select * from user where userID = ? and role = 1";
-            result = jdbcTemplate.query(asql, (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"), rs.getString("salt")), param);
-
         } else {
-            String sql = "select * from user where userID = ? and role = 1";
-            result = jdbcTemplate.query(sql, (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"),rs.getString("salt")), id);
-        }
-        if (result.size() <= 0)
-            return null;
-        return result.get(0);
-    }
-
-    public String getSalt(String username) {
-        List<User> result = new ArrayList<User>();
-
-        try {
-            result = jdbcTemplate.query("SELECT * FROM user WHERE username=?", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"), rs.getString("salt")), username);
-            if (result.size() == 0)
-                return null;
-            else {
-                return result.get(0).getSalt();
-            }
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    public List<User> findByUser(String username, String password) {
-        try {
-            List<User> result = new ArrayList<User>();
-
-            if (secSetting.getSqli()) {
-                result = jdbcTemplate.query("SELECT * FROM user WHERE username=? and password=?", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"), rs.getString("salt")), username, password);
-            } else {
-                result = jdbcTemplate.query("SELECT * FROM user WHERE username=? and password=?", (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"), rs.getString("salt")), username, password);
-            }
-
+            String sql = "select * from user where role = 1";
+            List<User> result = jdbcTemplate.query(sql, (rs, rowNum) -> new User(rs.getLong("userID"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("role"), rs.getString("salt")));
             return result;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return null;
+
+    } catch (Exception e) {
+        e.printStackTrace();
     }
-
-    public boolean changePassword(int id, String password) {
-        try {
-            jdbcTemplate.update("UPDATE user SET password = ? WHERE userID = ?", password, id);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }
-        return false;
-    }
-
-    public void updateAllSaltColumn(List<String> salts) {
-        try {
-            int id = 1;
-            for (String salt : salts) {
-                jdbcTemplate.update("UPDATE user SET salt = ? WHERE userID = ?", salt, id++);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void resetAllPassword(List<String> passwords) {
-        try {
-            int id = 1;
-            for (String password : passwords) {
-                jdbcTemplate.update("UPDATE user SET password = ? WHERE userID = ?", password, id++);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+    return null;
 }
 ```
--- test/java_server/src/main/java/com/mgmsec/HackMyTeeth/HackMyTeeth/dao/UserRepositoryTest.java
+++ test/java_server/src/main/java/com/mgmsec/HackMyTeeth/HackMyTeeth/dao/UserRepositoryTest.java
@@ -4,34 +4,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class UserRepositoryTest {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Test
-    public void testFindByUser() {
-        String username = "testUser";
-        String password = "testPassword";
-
-        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
-            String sql = "SELECT * FROM user WHERE username=? and password=?";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, username);
-            statement.setString(2, password);
-            ResultSet resultSet = statement.executeQuery();
-
-            // Assert the result
-            // ...
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public void testSearchDentist() {
+        String key = "John";
+        List<User> result = userRepository.searchDentist(key);
+        // Assert the result
     }
+
 }
