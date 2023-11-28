d219b062b6f157a804f4fe0691d377abd151fd51
Shubham Gangurde <shubhamgangurde16@gmail.com>
2023-11-28 15:31:55 +0530

    fix applied

---
+++ test/flask_server/simpleFlask.py
@@ -0,0 +1,24 @@
+import unittest
+from flask import Flask
+from flask.testing import FlaskClient
+from flask_cors import CORS, cross_origin
+import subprocess
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+        CORS(self.app)
+
+    def test_postSimpleApi_command_injection(self):
+        payload = {
+            'name': 'test; echo "Hacked!"'
+        }
+        response = self.client.post('/simpleApi', json=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn('Hacked!', response.get_data(as_text=True))
+
+if __name__ == '__main__':
+    unittest.main()
